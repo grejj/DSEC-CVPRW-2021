cd41c30e203290bbf08e71a4a405b153292c926f
grejj <grebej@uwindsor.ca>
2021-06-06 21:02:05 -0400

    initial commit

---
+++ estimator.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Estimator(nn.Module):
+
+    def __init__(self):
+        super(Estimator, self).__init__()
+        # 1 input image channel, 6 output channels, 5x5 square convolution
+        # kernel
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120) # 5x5 image dimension
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        # max pooling over a (2,2) window
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2,2))
+        # if size is a square, can specify with a single number
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        # flatten all dimensions except the batch dimension 
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+estimator = Estimator()
+print(estimator)
---
+++ predictor.py
@@ -0,0 +1,4 @@
+'''
+Predictor loads data and does the training and testing
+'''
+
